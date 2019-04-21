03b3c769e618caa851fb255b135fc7f488f52c97
wh6636 <wh6636@localhost.localdomain>
2019-04-21 20:57:16 +0800

    1

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Model\GoodsModel;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+use App\Model\CartModel;
+class CartController extends Controller
+{
+    //购物车页面
+}
