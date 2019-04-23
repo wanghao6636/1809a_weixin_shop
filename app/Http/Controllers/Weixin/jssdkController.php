f0476ef5d1da86611976ed3b7e4fe370d65c5acf
wh6636 <wh6636@localhost.localdomain>
2019-04-23 23:24:44 +0800

    改

--- app/Http/Controllers/Weixin/jssdkController.php
+++ app/Http/Controllers/Weixin/jssdkController.php
@@ -13,7 +13,7 @@ class jssdkController extends Controller{
         //var_dump($aa);exit;
         $timestamp=time();
        // var_dump($_SERVER['REQUEST_URL']);exit;
-        $current_url=$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URL'];
+        $current_url=$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 
 
 
@@ -25,18 +25,18 @@ class jssdkController extends Controller{
 
 
         $js_config=[
-            'appId'=> env(WX_APP_ID), // 必填，公众号的唯一标识
+            'appId'=> env('WX_APP_ID'), // 必填，公众号的唯一标识
             'timestamp'=> $timestamp, // 必填，生成签名的时间戳
             'nonceStr'=>$nonceStr, // 必填，生成签名的随机串
             'signature'=>$sign,// 必填，签名
         ];
         $data=[
-            jsApiList=>$js_config // 必填，需要使用的JS接口列表
+            'js_config'=>$js_config // 必填，需要使用的JS接口列表
         ];
         return view('weixin.jssdk',$data);
     }
     public function getImg()
     {
-        echo print_r();
+        echo '<pre>';print_r($_GET);echo '</pre>';
     }
 }
