9a9019482dc5feee21e1fb2bedaab62661941853
wh6636 <wh6636@localhost.localdomain>
2019-04-25 00:06:33 +0800

    微信回调

--- app/Http/Controllers/Weixin/ZffController.php
+++ app/Http/Controllers/Weixin/ZffController.php
@@ -92,13 +92,13 @@ class ZffController extends Controller
         //var_dump($arr);exit;
 
         $code = $arr['code'];
-        $user_id = '15';
+        //$user_id = '15';
         $appid = "wxf45738393e3e870a";
         $appkey = "04c57ee962b7bf78d85050ce9d213833";
         $accessToken = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$appkey&code=$code&grant_type=authorization_code";
         $info = file_get_contents($accessToken);
         $arr = json_decode($info,true);
-        //var_dump($arr);exit;
+        var_dump($arr);exit;
         $openid = $arr['openid'];
         $userUrl = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=$access&openid=$openid&lang=zh_CN";
         $userAccessInfo = file_get_contents($userUrl);
